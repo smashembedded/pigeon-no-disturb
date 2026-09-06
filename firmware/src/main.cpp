66002c7054d477f6443fe96082d865097b8b06a8
smashembedded <smashembedded@gmail.com>
2026-09-06 18:47:19 +0200

    feat: implement HC-SR04 distance measurement (#1)

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,15 +1,55 @@
 #include <Arduino.h>
 
+namespace Pins
+{
+constexpr uint8_t TRIG = 5;
+constexpr uint8_t ECHO = 18;
+}
+
+namespace Ultrasonic
+{
+void begin()
+{
+    pinMode(Pins::TRIG, OUTPUT);
+    pinMode(Pins::ECHO, INPUT);
+
+    digitalWrite(Pins::TRIG, LOW);
+}
+
+float readDistanceCm()
+{
+    digitalWrite(Pins::TRIG, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(Pins::TRIG, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(Pins::TRIG, LOW);
+
+    const unsigned long duration = pulseIn(Pins::ECHO, HIGH);
+
+    return duration / 58.0f;
+}
+}
+
 void setup()
 {
     Serial.begin(115200);
 
+    Ultrasonic::begin();
+
     Serial.println();
     Serial.println("Pigeon No Disturb");
-    Serial.println("System starting...");
+    Serial.println("Ultrasonic sensor ready");
 }
 
 void loop()
 {
-    delay(1000);
+    const float distanceCm = Ultrasonic::readDistanceCm();
+
+    Serial.print("Distance: ");
+    Serial.print(distanceCm);
+    Serial.println(" cm");
+
+    delay(400);
 }
